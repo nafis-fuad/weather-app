a6baeef63280fedd5da84de029060e67c35d7922
Nafis Fuad <fuadbd14@gmail.com>
2019-05-26 23:41:58 +0600

    updated path for key requirement

--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,59 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const request = require("request");
-const api = require("../api.json");
-const apiKey = api.key;
+const request = require('request');
+const config = require('../config.js');
+const apiKey = config.key;
 
-
-router.get("/", (req, res) => {
-  return res.render("index");
+router.get('/', (req, res) => {
+	return res.render('index');
 });
 
 router.post('/', (req, res) => {
-  let city = req.body.placename;
-  let url = `https://api.apixu.com/v1/current.json?key=${apiKey}&q=${city}`;
+	let city = req.body.placename;
+	let url = `https://api.apixu.com/v1/current.json?key=${apiKey}&q=${city}`;
 
-  request(url, (err, response, body) => {
-    if (err) {
-      res.render('error');
-    } else {
-      let weather = JSON.parse(body);
-      if (response.statusCode !== 200) {
-        res.render('error');
-      } else {
-        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-        res.setHeader("Pragma", "no-cache");
-        res.setHeader("Expires", 0);
+	request(url, (err, response, body) => {
+		if (err) {
+			res.render('error');
+		} else {
+			let weather = JSON.parse(body);
+			if (response.statusCode !== 200) {
+				res.render('error');
+			} else {
+				res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+				res.setHeader('Pragma', 'no-cache');
+				res.setHeader('Expires', 0);
 
-        const locationName = weather.location.name;
-        const locationCountry = weather.location.country;
-        const currentWeather = weather.current;
-        const currentIsDay = currentWeather.is_day;
-        const currentTemperature = currentWeather.temp_c;
-        const currentConditionMessage = currentWeather.condition.text;
-        const currentPrecipitation = currentWeather.precip_mm;
-        const currentHumidity = currentWeather.humidity;
-        const currentWind = currentWeather.wind_mph;
-        const currentDateAndTime = response.headers.date;
-        const currentConditionIconCode = currentWeather.condition.code;
-        const templateData = {locationName, locationCountry, 
-                              currentTemperature, currentConditionMessage, 
-                              currentPrecipitation, currentHumidity, 
-                              currentWind, currentDateAndTime,
-                              currentIsDay, currentConditionIconCode};
+				const locationName = weather.location.name;
+				const locationCountry = weather.location.country;
+				const currentWeather = weather.current;
+				const currentIsDay = currentWeather.is_day;
+				const currentTemperature = currentWeather.temp_c;
+				const currentConditionMessage = currentWeather.condition.text;
+				const currentPrecipitation = currentWeather.precip_mm;
+				const currentHumidity = currentWeather.humidity;
+				const currentWind = currentWeather.wind_mph;
+				const currentDateAndTime = response.headers.date;
+				const currentConditionIconCode = currentWeather.condition.code;
+				const templateData = {
+					locationName,
+					locationCountry,
+					currentTemperature,
+					currentConditionMessage,
+					currentPrecipitation,
+					currentHumidity,
+					currentWind,
+					currentDateAndTime,
+					currentIsDay,
+					currentConditionIconCode
+				};
 
-        // if (currentIsDay === 1) {
-        //   return res.render("day", templateData );
-        // } else {
-        //   return res.render("night", templateData );
-        // }
-          
-        res.render("current", templateData);
-
-      }
-    }
-  });
+				res.render('current', templateData);
+			}
+		}
+	});
 });
-
 
 module.exports = router;
